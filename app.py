9eeecdd735c62a39bd996fa7fba57f02c5c65cf6
Chuma Meyiswa <nmeyiswa@gmail.com>
2025-05-09 20:45:58 +0200

    fixed whatsapp bot function, new listings

--- app.py
+++ app.py
@@ -22,7 +22,10 @@ def wahtsapp():
     
     found = False
     reply = ""
+    the_city = None
+    the_category = None
     
+    #Supported Cities
     cities = ["johannesburg", "pretoria", "soweto"]
     
     categories = {
@@ -34,14 +37,39 @@ def wahtsapp():
     for city in cities:
         if city in incoming_msg:
             the_city = city
+            break
             
     for category, value_words in categories.items():
         for word in value_words:
             if word in incoming_msg:
                 the_category = category
+                break
     
     if the_city and the_category:
         results = []
+        for item in listings:
+            if item["category"] == the_category and item["city"] == the_city:
+                results.append(item)
+    
+        if results:
+            reply = f"{the_category} listings in {the_city}:\n"
+            for r in results:
+                reply += f"{r['title']} - {r['price']} (call: {r['contact']})\n"
+        else:
+            reply += f"No {the_category}s found in {the_city}"
+    
+    else:
+        reply = (
+            "👋 Welcome to LinkUp!\n"
+            "Type something like:\n"
+            "`house in Soweto`\n"
+            "`job in Johannesburg`\n"
+            "`service in Pretoria`\n"
+        )
+        
+    msg.body(reply)
+    return str(response)
+                
     
 
 
